654a293015e4384361a61cdaecbbbf780502bf29
Paul Cheteles <paul.cheteles@reea.net>
2016-02-10 12:36:02 +0200

    Bust varnish

--- docroot/sites/default/settings.php
+++ docroot/sites/default/settings.php
@@ -602,3 +602,6 @@ if (file_exists('/var/www/site-php')) {
   require('/var/www/site-php/transylvania/transylvania-settings.inc');
 }
 
+
+// Fuck you Varnish cache!
+drupal_add_http_header('Cache-Control', 'public, max-age=0');
