a9dfe60bca8a96009a9c813253a3a0d1c147ebd2
drupaltransylvania_meli <melinda.csog@reea.net>
2017-03-31 11:33:58 +0300

    invoice data

--- docroot/sites/all/modules/custom/dctransylvania_invoice/includes/commerce_order--commerce_order--pdf.tpl.php
+++ docroot/sites/all/modules/custom/dctransylvania_invoice/includes/commerce_order--commerce_order--pdf.tpl.php
@@ -3,9 +3,9 @@
     <div class="invoice-header"><?php print render($content['invoice_header']); ?></div>
   </div>
   <div class="invoice-text"><?php print render($content['invoice_text']); ?></div>
-  <div class="line-items">
+  <!--<div class="line-items">
     <div class="line-items-view"><?php print render($content['commerce_line_items']); ?></div>
-  </div>
+  </div>-->
   <div class="footer"><?php print render($content['invoice_footer']); ?></div>
 </div>
 
