2bc7b25d014dcccdca223a1c51d9f5fd5424dcb4
AlexandruSateanuHoria <alexandru.sateanu@pitechnologies.ro>
2017-10-04 15:35:33 +0300

    Header done

--- _html/templates/modules/header.php
+++ _html/templates/modules/header.php
@@ -1,2 +1,9 @@
-<header></header>
+<header class="header">
+    <div class="header-logo">
+        <a href="javascript:void(0)" title="Homepage"></a>
+    </div>
+    <div class="header-login">
+        <a href="javascript:void(0)" title="Log in">Log in</a>
+    </div>
+</header>
 <main>
---
+++ _html/templates/sponsors.php
@@ -0,0 +1,13 @@
+<?php 
+/** 
+* Homepage template.
+*/
+
+include "inc/header.inc.php";
+include "modules/header.php";
+
+include "modules/sponsors-intro.php";
+
+include "modules/footer.php";
+include "inc/footer.inc.php";
+?>
