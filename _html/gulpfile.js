87a4df33d3910e022c1b2b10a41427bee6ba6052
AlexandruSateanuHoria <alexandru.sateanu@pitechnologies.ro>
2017-10-06 13:51:18 +0300

    Refactor header and footer. Fix a gulp warning issue

--- _html/gulpfile.js
+++ _html/gulpfile.js
@@ -36,12 +36,12 @@ gulp.task('sass', function() {
 				'merge-default-rules': true
 			},
 			rules: {
-				'indentation': ['1', {'size': '4'}],
-				'property-sort-order': ['1', {'order': 'smacss'}],
-				'no-ids': '0',
-				'no-url-domains': '0',
-				'no-url-protocols': '0',
-				'mixins-before-declarations': '0'
+				'indentation': [1, {'size': '4'}],
+				'property-sort-order': [1, {'order': 'smacss'}],
+				'mixins-before-declarations': [1, {'exclude': ['tablet', 'desktop']}],
+				'no-ids': 0,
+				'no-url-domains': 0,
+				'no-url-protocols': 0
 			},
 			files: {ignore: 'scss/base/_reset.scss'}	
 		}))
