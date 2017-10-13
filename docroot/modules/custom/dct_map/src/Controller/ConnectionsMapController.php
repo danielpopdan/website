61de4411112657f131c49df755dac6b3ddcf17db
Hangea Cristian <cristian.hangea@pitechnologies.ro>
2017-10-13 15:18:47 +0300

    Integrate Mapael js library. See #59.

---
+++ docroot/modules/custom/dct_map/src/Controller/ConnectionsMapController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Drupal\dct_map\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+use Drupal\dct_airport_connections\DataProviderInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Drupal\dct_airport_connections;
+
+class ConnectionsMapController extends ControllerBase {
+
+
+  /**
+   * Data provider the for airport connections.
+   *
+   * @var \Drupal\dct_airport_connections\DataProviderInterface
+   */
+  protected $connectionsDataProvider;
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('dct_airport_connections.data_provider')
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function __construct(DataProviderInterface $dataProvider) {
+    $this->connectionsDataProvider = $dataProvider;
+  }
+
+  /**
+   * Returns the array associated to the airport connections map.
+   *
+   * @return array
+   */
+  public function getMap() {
+
+
+    return[
+      '#theme' => 'dct_map_connections_map',
+      '#attached' => [
+        'library' => [
+          'dct_map/mapael',
+          'dct_map/maps_eu',
+          'dct_theme/connections-map',
+        ],
+        'drupalSettings' => [
+          'connections-map' => [
+            'plots' => $this->connectionsDataProvider->getPlots(),
+            'links' => $this->connectionsDataProvider->getLinks(),
+          ],
+        ],
+      ],
+    ];
+  }
+
+}
