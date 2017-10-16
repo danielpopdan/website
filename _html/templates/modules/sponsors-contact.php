b6d0f86559ed525c6cf699b049e13fdaabaf0501
AlexandruSateanuHoria <alexandru.sateanu@pitechnologies.ro>
2017-10-16 15:38:49 +0300

    Sponsors contact form template. See DCT-17

---
+++ _html/templates/modules/sponsors-contact.php
@@ -0,0 +1,56 @@
+<div class="sponsors-contact">
+    <div class="l-container">
+        <h2 class="sponsors-contact--title">Media Partnership</h2>
+        <p class="sponsors-contact--intro">You can also become our media partner! Help us reach a broader audience and spread the word to your followers! In exchange, your logo will be featured on the website.</p>
+        <h2 class="sponsors-contact--form-title">Contact Us</h2>
+    
+        <form class="contact-message-sponsors-contact-form-form contact-message-form contact-form" data-drupal-selector="contact-message-sponsors-contact-form-form" action="/contact/sponsors_contact_form" method="post" id="contact-message-sponsors-contact-form-form" accept-charset="UTF-8">
+            <div class="field--type-string field--name-field-name field--widget-string-textfield js-form-wrapper form-wrapper" data-drupal-selector="edit-field-name-wrapper" id="edit-field-name-wrapper">
+                <div class="js-form-item form-item js-form-type-textfield form-type-textfield js-form-item-field-name-0-value form-item-field-name-0-value">
+                    <label for="edit-field-name-0-value" class="js-form-required form-required">Name</label>
+                    <input class="js-text-full text-full form-text required" data-drupal-selector="edit-field-name-0-value" aria-describedby="edit-field-name-0-value--description" type="text" id="edit-field-name-0-value" name="field_name[0][value]" value="" size="60" maxlength="255" placeholder="" required="required" aria-required="true" />
+    
+                    <div id="edit-field-name-0-value--description" class="description">
+                        The name of the sponsor.
+                    </div>
+                </div>
+            </div>
+            <input autocomplete="off" data-drupal-selector="form-z6rq4se3-dis2pcy5m-tnz5cyjmrblzr4ili-lyocu" type="hidden" name="form_build_id" value="form-Z6Rq4sE3-Dis2pCy5M-tnZ5cyjMrblzr4ilI_-LyOCU" />
+            <input data-drupal-selector="edit-contact-message-sponsors-contact-form-form-form-token" type="hidden" name="form_token" value="Ok8SZZmhe4esKq5QY9_OAMQyIHsD0OSIm75deN4Sybg" />
+            <input data-drupal-selector="edit-contact-message-sponsors-contact-form-form" type="hidden" name="form_id" value="contact_message_sponsors_contact_form_form" />
+            <div class="field--type-email field--name-field-email field--widget-email-default js-form-wrapper form-wrapper" data-drupal-selector="edit-field-email-wrapper" id="edit-field-email-wrapper">
+                <div class="js-form-item form-item js-form-type-email form-type-email js-form-item-field-email-0-value form-item-field-email-0-value">
+                    <label for="edit-field-email-0-value" class="js-form-required form-required">Email</label>
+                    <input data-drupal-selector="edit-field-email-0-value" aria-describedby="edit-field-email-0-value--description" type="email" id="edit-field-email-0-value" name="field_email[0][value]" value="" size="60" maxlength="254" placeholder="" class="form-email required" required="required" aria-required="true" />
+    
+                    <div id="edit-field-email-0-value--description" class="description">
+                        The email of the sponsor.
+                    </div>
+                </div>
+            </div>
+            <div class="field--type-string field--name-subject field--widget-string-textfield js-form-wrapper form-wrapper" data-drupal-selector="edit-subject-wrapper" id="edit-subject-wrapper">
+                <div class="js-form-item form-item js-form-type-textfield form-type-textfield js-form-item-subject-0-value form-item-subject-0-value">
+                    <label for="edit-subject-0-value" class="js-form-required form-required">Subject</label>
+                    <input class="js-text-full text-full form-text required" data-drupal-selector="edit-subject-0-value" type="text" id="edit-subject-0-value" name="subject[0][value]" value="" size="60" maxlength="100" placeholder="" required="required" aria-required="true"/>
+                </div>
+            </div>
+            <div class="field--type-language field--name-langcode field--widget-language-select js-form-wrapper form-wrapper" data-drupal-selector="edit-langcode-wrapper" id="edit-langcode-wrapper">
+            </div>
+            <div class="field--type-string-long field--name-field-message field--widget-string-textarea js-form-wrapper form-wrapper" data-drupal-selector="edit-field-message-wrapper" id="edit-field-message-wrapper">
+                <div class="js-form-item form-item js-form-type-textarea form-type-textarea js-form-item-field-message-0-value form-item-field-message-0-value">
+                    <label for="edit-field-message-0-value" class="js-form-required form-required">Message</label>
+                    <div class="form-textarea-wrapper">
+                        <textarea class="js-text-full text-full form-textarea required resize-vertical" data-drupal-selector="edit-field-message-0-value" aria-describedby="edit-field-message-0-value--description" id="edit-field-message-0-value" name="field_message[0][value]" rows="5" cols="60" placeholder="" required="required" aria-required="true"></textarea>
+                    </div>
+                    <div id="edit-field-message-0-value--description" class="description">
+                        The message.
+                    </div>
+                </div>
+            </div>
+            <div data-drupal-selector="edit-actions" class="form-actions js-form-wrapper form-wrapper" id="edit-actions">
+                <input data-drupal-selector="edit-submit" type="submit" id="edit-submit" name="op" value="Send message" class="button button--primary js-form-submit form-submit" />
+                <input data-drupal-selector="edit-preview" type="submit" id="edit-preview" name="op" value="Preview" class="button js-form-submit form-submit" />
+            </div>
+        </form>
+    </div>
+</div>
