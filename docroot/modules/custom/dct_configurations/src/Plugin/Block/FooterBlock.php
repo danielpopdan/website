a4eeeb0b9554852031ae4155700079f1dd423053
alina.cornea <alina.cornea@pitechnologies.ro>
2017-10-20 15:55:38 +0300

    Footer integration update. See #92.

---
+++ docroot/modules/custom/dct_configurations/src/Plugin/Block/FooterBlock.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Drupal\dct_configurations\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Provides Footer Block.
+ *
+ * @Block(
+ *   id = "footer_block",
+ *   admin_label = @Translation("Footer Block"),
+ * )
+ */
+class FooterBlock extends BlockBase implements ContainerFactoryPluginInterface {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    return [
+      '#theme' => 'footerblock',
+    ];
+  }
+
+}
