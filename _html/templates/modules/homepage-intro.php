8581f71580aabe1e890a71de6ccb5b7fe5d31967
AlexandruSateanuHoria <alexandru.sateanu@pitechnologies.ro>
2017-10-23 12:29:20 +0300

    Homepage - update parallax and intro. See DCT-110

--- _html/templates/modules/homepage-intro.php
+++ _html/templates/modules/homepage-intro.php
@@ -15,6 +15,10 @@
             <h3 class="homepage-intro--details--location">Cluj-Napoca, The Office</h3>
         </div>
         <div class="homepage-intro--interested-form">
+            <div class="homepage-intro--interested-form--intro">
+                <h2>Subscribe to our Newsletter</h2>
+                <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit.</p>
+            </div>
             <form action="#" method="POST">
                 <input type="email" name="contact-email" placeholder="Email address">
                 <button type="submit" name="contact-submit" value="I'm interested">I'm interested</button>
