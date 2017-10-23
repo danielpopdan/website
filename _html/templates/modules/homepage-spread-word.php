89d932e069b52accc65132df974d36ecebcb8d2f
AlexandruSateanuHoria <alexandru.sateanu@pitechnologies.ro>
2017-10-23 18:08:57 +0300

    Sponsors structure changes. See DCT-116

--- _html/templates/modules/homepage-spread-word.php
+++ _html/templates/modules/homepage-spread-word.php
@@ -1,6 +1,6 @@
 <div class="homepage-spread-word">
     <div class="l-container">
-        <div class="homepage-spread-word--intro title-block">
+        <div class="homepage-spread-word--intro large-title-block">
             <h2>Spread the word!</h2>
             <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit.</p>
         </div>
