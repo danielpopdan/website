e392163a2f952f99bbcb6089e7ae0f7e4f6268dc
Ovidiu-Dan Bojor <ovidiu.bojor@office.pitechnologies.ro>
2017-12-14 11:53:15 +0200

    Mentors page with form.

---
+++ _html/templates/modules/mentors-contact.php
@@ -0,0 +1,104 @@
+<div class="mentors-form">
+  <form class="contact-message-sprint-mentor-form-form contact-message-form contact-form" data-drupal-selector="contact-message-sprint-mentor-form-form" action="/contact/sprint_mentor_form" method="post" id="contact-message-sprint-mentor-form-form" accept-charset="UTF-8" data-drupal-form-fields="edit-field-first-name-0-value,edit-field-last-name-0-value,edit-field-drupal-org-username-0-value,edit-field-organization-0-target-id,edit-field-gender-m,edit-field-gender-f,edit-field-mentored-before-y,edit-field-mentored-before-n,edit-field-message-0-value,edit-field-shirt-size,edit-submit,edit-preview">
+  <div class="field--type-string field--name-field-first-name field--widget-string-textfield js-form-wrapper form-wrapper" data-drupal-selector="edit-field-first-name-wrapper" id="edit-field-first-name-wrapper">
+    <div class="js-form-item form-item js-form-type-textfield form-type-textfield js-form-item-field-first-name-0-value form-item-field-first-name-0-value">
+      <label for="edit-field-first-name-0-value" class="js-form-required form-required">First Name<i>*</i></label>
+      <input class="js-text-full text-full form-text required" data-drupal-selector="edit-field-first-name-0-value" type="text" id="edit-field-first-name-0-value" name="field_first_name[0][value]" value="" size="60" maxlength="255" placeholder="" required="required" aria-required="true">
+    </div>
+  </div>
+  <div class="field--type-string field--name-field-last-name field--widget-string-textfield js-form-wrapper form-wrapper" data-drupal-selector="edit-field-last-name-wrapper" id="edit-field-last-name-wrapper">
+    <div class="js-form-item form-item js-form-type-textfield form-type-textfield js-form-item-field-last-name-0-value form-item-field-last-name-0-value">
+      <label for="edit-field-last-name-0-value" class="js-form-required form-required">Last Name<i>*</i></label>
+      <input class="js-text-full text-full form-text required" data-drupal-selector="edit-field-last-name-0-value" type="text" id="edit-field-last-name-0-value" name="field_last_name[0][value]" value="" size="60" maxlength="255" placeholder="" required="required" aria-required="true">
+    </div>
+  </div>
+  <div class="field--type-string field--name-field-drupal-org-username field--widget-string-textfield js-form-wrapper form-wrapper" data-drupal-selector="edit-field-drupal-org-username-wrapper" id="edit-field-drupal-org-username-wrapper">
+    <div class="js-form-item form-item js-form-type-textfield form-type-textfield js-form-item-field-drupal-org-username-0-value form-item-field-drupal-org-username-0-value">
+      <label for="edit-field-drupal-org-username-0-value">Drupal.org username<i>*</i></label>
+      <input class="js-text-full text-full form-text" data-drupal-selector="edit-field-drupal-org-username-0-value" type="text" id="edit-field-drupal-org-username-0-value" name="field_drupal_org_username[0][value]" value="" size="60"maxlength="255" placeholder="">
+    </div>
+  </div>
+  <div class="field--type-string field--name-field-drupal-org-username field--widget-string-textfield js-form-wrapper form-wrapper" data-drupal-selector="edit-field-drupal-org-username-wrapper" id="edit-field-drupal-org-username-wrapper">
+    <div class="js-form-item form-item js-form-type-textfield form-type-textfield js-form-item-field-drupal-org-username-0-value form-item-field-drupal-org-username-0-value">
+      <label for="edit-field-drupal-org-username-0-value">Email adress<i>*</i></label>
+      <input class="js-text-full text-full form-text" data-drupal-selector="edit-field-drupal-org-username-0-value" type="email" id="edit-field-drupal-org-username-0-value" name="field_drupal_org_username[0][value]" value="" size="60"maxlength="255" placeholder="">
+    </div>
+  </div>
+  <div class="field--type-entity-reference field--name-field-organization field--widget-entity-reference-autocomplete js-form-wrapper form-wrapper" data-drupal-selector="edit-field-organization-wrapper" id="edit-field-organization-wrapper">
+    <div class="js-form-item form-item js-form-type-entity-autocomplete form-type-entity-autocomplete js-form-item-field-organization-0-target-id form-item-field-organization-0-target-id">
+      <label for="edit-field-organization-0-target-id">Organization<i>*</i></label>
+      <input data-drupal-selector="edit-field-organization-0-target-id" class="form-autocomplete form-text ui-autocomplete-input" data-autocomplete-path="/entity_reference_autocomplete/taxonomy_term/default%3Ataxonomy_term/nEHDeU9ugnDI5v8Q8yBAhOBxQAAtSUEuQF0PzzkZr7Q" type="text" id="edit-field-organization-0-target-id" name="field_organization[0][target_id]" value="" size="60" maxlength="1024" placeholder="" autocomplete="off">
+    </div>
+  </div>
+  <div class="field--type-list-string field--name-field-gender field--widget-options-buttons js-form-wrapper form-wrapper" data-drupal-selector="edit-field-gender-wrapper" id="edit-field-gender-wrapper">
+    <fieldset data-drupal-selector="edit-field-gender" id="edit-field-gender--wrapper" class="fieldgroup form-composite required js-form-item form-item js-form-wrapper form-wrapper" required="required" aria-required="true">
+      <legend>
+        <span class="fieldset-legend js-form-required form-required">Gender<i>*</i></span>
+      </legend>
+      <div class="fieldset-wrapper">
+        <div id="edit-field-gender" class="form-radios">
+          <div class="js-form-item form-item js-form-type-radio form-type-radio js-form-item-field-gender form-item-field-gender">
+            <input data-drupal-selector="edit-field-gender-m" type="radio" id="edit-field-gender-m" name="field_gender" value="M" class="form-radio">
+            <label for="edit-field-gender-m" class="option">Male</label>
+          </div>
+          <div class="js-form-item form-item js-form-type-radio form-type-radio js-form-item-field-gender form-item-field-gender">
+            <input data-drupal-selector="edit-field-gender-f" type="radio" id="edit-field-gender-f" name="field_gender" value="F" class="form-radio">
+            <label for="edit-field-gender-f" class="option">Female</label>
+          </div>
+        </div>
+      </div>
+    </fieldset>
+  </div>
+  <div class="field--type-list-string field--name-field-shirt-size field--widget-options-select js-form-wrapper form-wrapper" data-drupal-selector="edit-field-shirt-size-wrapper" id="edit-field-shirt-size-wrapper">
+    <div class="js-form-item form-item js-form-type-select form-type-select js-form-item-field-shirt-size form-item-field-shirt-size">
+      <label for="edit-field-shirt-size" class="js-form-required form-required">Sprint mentor shirt size<i>*</i></label>
+      <select data-drupal-selector="edit-field-shirt-size" id="edit-field-shirt-size" name="field_shirt_size" class="form-select required" required="required" aria-required="true">
+        <option value="" selected="selected">- Select -</option>
+        <option value="_none">- Select a value -</option>
+        <option value="xs">XS</option>
+        <option value="s">S</option>
+        <option value="m">M</option>
+        <option value="l">L</option>
+        <option value="xl">XL</option>
+        <option value="xxl">XXL</option>
+      </select>
+    </div>
+  </div>
+
+  <div class="field--type-list-string field--name-field-mentored-before field--widget-options-buttons js-form-wrapper form-wrapper" data-drupal-selector="edit-field-mentored-before-wrapper" id="edit-field-mentored-before-wrapper">
+    <fieldset data-drupal-selector="edit-field-mentored-before" id="edit-field-mentored-before--wrapper" class="fieldgroup form-composite required js-form-item form-item js-form-wrapper form-wrapper" required="required" aria-required="true">
+      <legend>
+        <span class="fieldset-legend js-form-required form-required">Have you mentored a Drupal sprint before?<i>*</i></span>
+      </legend>
+      <div class="fieldset-wrapper">
+        <div id="edit-field-mentored-before" class="form-radios">
+          <div class="js-form-item form-item js-form-type-radio form-type-radio js-form-item-field-mentored-before form-item-field-mentored-before">
+            <input data-drupal-selector="edit-field-mentored-before-y" type="radio" id="edit-field-mentored-before-y" name="field_mentored_before" value="y" class="form-radio">
+            <label for="edit-field-mentored-before-y" class="option">Yes</label>
+          </div>
+          <div class="js-form-item form-item js-form-type-radio form-type-radio js-form-item-field-mentored-before form-item-field-mentored-before">
+            <input data-drupal-selector="edit-field-mentored-before-n" type="radio" id="edit-field-mentored-before-n" name="field_mentored_before" value="n" class="form-radio">
+            <label for="edit-field-mentored-before-n" class="option">No</label>
+          </div>
+        </div>
+      </div>
+    </fieldset>
+  </div>
+
+  <div class="field--type-string-long field--name-field-message field--widget-string-textarea js-form-wrapper form-wrapper" data-drupal-selector="edit-field-message-wrapper" id="edit-field-message-wrapper">
+    <div class="js-form-item form-item js-form-type-textarea form-type-textarea js-form-item-field-message-0-value form-item-field-message-0-value">
+      <label for="edit-field-message-0-value">Message<i>*</i></label>
+      <div class="form-textarea-wrapper">
+        <textarea class="js-text-full text-full form-textarea resize-vertical" data-drupal-selector="edit-field-message-0-value" id="edit-field-message-0-value" name="field_message[0][value]" rows="5" cols="60" placeholder=""></textarea>
+      </div>
+    </div>
+  </div>
+  <input autocomplete="off" data-drupal-selector="form-ep7hpsuzhv8lvcsyz1mefsijylkhxlampvtqfx1wolw" type="hidden" name="form_build_id" value="form-Ep7HpsUZhV8lvCsyZ1MEFsIJYlkHxLAmpvtqFX1wolw" />
+  <input data-drupal-selector="edit-contact-message-sprint-mentor-form-form-form-token" type="hidden" name="form_token" value="8JJenfPCXEjFuc_m18rrRXLUNj-u-nsOAVTeTJjRcRI" />
+  <input data-drupal-selector="edit-contact-message-sprint-mentor-form-form" type="hidden" name="form_id" value="contact_message_sprint_mentor_form_form" />
+  <div data-drupal-selector="edit-actions" class="form-actions js-form-wrapper form-wrapper" id="edit-actions">
+    <input data-drupal-selector="edit-submit" type="submit" id="edit-submit" name="op" value="Send message" class="button button--primary js-form-submit form-submit">
+    <input data-drupal-selector="edit-preview" type="submit" id="edit-preview" name="op" value="Preview" class="button js-form-submit form-submit">
+  </div>
+  </form>
+</div>
---
+++ _html/templates/sprint-mentors.php
@@ -0,0 +1,25 @@
+<?php
+/**
+* Sponsors template.
+*/
+
+include "inc/header.inc.php";
+include "modules/header.php";
+?>
+
+<div class="section-intro sponsors-intro">
+    <div class="section-intro--background"></div>
+    <div class="section-intro--text">
+        <h1 class="section-intro--title">Be a sprint mentor</h1>
+        <div class="section-intro--details text-center">
+            <p>Introduction text consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exevoluptatem.</p>
+        </div>
+    </div>
+</div>
+
+<?php
+include "modules/mentors-contact.php";
+
+include "modules/footer.php";
+include "inc/footer.inc.php";
+?>
