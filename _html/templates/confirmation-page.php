a305096c2a443c41d05b69d646421155d94d9396
vlad.gheorghita <vlad.gheorghita@pitechplus.com>
2018-01-12 13:38:45 +0200

    Confirmation page. See #323.

---
+++ _html/templates/confirmation-page.php
@@ -0,0 +1,12 @@
+<?php
+/**
+* Homepage template.
+*/
+
+include "inc/header.inc.php";
+include "modules/header.php";
+
+include "modules/confirmation-content.php";
+
+include "inc/footer.inc.php";
+?>
