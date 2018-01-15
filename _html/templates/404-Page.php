70efd3c48f9cd0fc4446cc3bf97397f04e83d68e
vlad.gheorghita <vlad.gheorghita@pitechplus.com>
2018-01-15 15:41:57 +0200

    Error-page. See #318.

---
+++ _html/templates/404-Page.php
@@ -0,0 +1,9 @@
+<?php
+
+include "inc/header.inc.php";
+include "modules/header.php";
+
+include "modules/error-content.php";
+
+include "inc/footer.inc.php";
+?>
---
+++ _html/templates/modules/error-content.php
@@ -0,0 +1,12 @@
+<div class="error-page-container">
+    <img src="../images/backgrounds/404-page-bg.png" alt="background">
+    <div class="image-text-wrapper">
+        <img src="../images/backgrounds/error.png" alt="message-image">
+        <div class="text-btn-wrapper">
+            <p>Oops! Beware of vampires.</br>
+                It seems you’re lost in Transylvania...
+            </p>
+            <button type="button" class="button button-primary">GO BACK HOME</button>
+        </div>
+    </div>
+</div>
