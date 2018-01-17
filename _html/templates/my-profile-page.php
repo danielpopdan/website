8a6674dd2f3f4205b800b579655c6756a8f88266
Andrei Bajenaru <andrei.bajenaru@pitechnologies.ro>
2018-01-17 19:32:03 +0200

    My profile page structure. See #319.

---
+++ _html/templates/my-profile-page.php
@@ -0,0 +1,20 @@
+<?php
+/**
+* My profile page - view mode.
+*/
+
+include "inc/header.inc.php";
+include "modules/header.php";
+?>
+
+<div class="section-intro sponsors-intro">
+    <div class="section-intro--background"></div>
+</div>
+
+
+
+<?php
+include "modules/my-profile.php";
+include "modules/footer-full-color.php";
+include "inc/footer.inc.php";
+?>
