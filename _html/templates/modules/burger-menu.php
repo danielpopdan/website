fcf9e00268f6231669f3c10dd497cc6c18250380
vlad.gheorghita <vlad.gheorghita@pitechplus.com>
2018-01-18 09:43:59 +0200

    Changed structure to desired. Added arrows. See #308.

--- _html/templates/modules/burger-menu.php
+++ _html/templates/modules/burger-menu.php
@@ -2,40 +2,85 @@
     <div class="btn-layers"></div>
 </div>
 <div class="l-container burger-menu">
-    <ul>
-        <li>
-            <span class="action-link">Homepage</span>
-        </li>
-        <li>
-            <span class="action-link">Event</span>
-        </li>
-        <li>
-            <span class="action-link">Registration</span>
-        </li>
-        <li>
-            <span class="action-link">Program</span>
-            <ul class="sublinks">
-                <li>Schedule</li>
-                <li>Birds of Feather</li>
-                <li>Sprints</li>
-                <li>Social Events</li>
-                <li>Accepted Sessions</li>
-                <li>Session Tracks</li>
-            </ul>
-        </li>
-        <li>
-            <span class="action-link">Travel</span>
-        </li>
-        <li>
-            <span class="action-link">Community</span>
-        </li>
-        <li>
-            <span class="action-link">Sponsors</span>
-        </li>
-        <li>
-            <span class="action-link">Login/My Account</span>
-        </li>
-    </ul>
+    <nav role="navigation" aria-labelledby="block-burgermenu-menu" id="block-burgermenu" class="block block-menu navigation menu--burger">
+        <h2 class="visually-hidden" id="block-burgermenu-menu">Burger Menu</h2>
+        <ul class="menu">
+            <li class="menu-item">
+                <a href="/" data-drupal-link-system-path="<front>" class="action-link">Homepage</a>
+            </li>
+            <li class="menu-item menu-item--expanded">
+                <a href="/" data-drupal-link-system-path="<front>" class="action-link">Event</a>
+                <ul class="menu sublinks">
+                    <li class="menu-item">
+                        <a href="/node/4" data-drupal-link-system-path="node/4">About Us</a>
+                    </li>
+                    <li class="menu-item">
+                        <a href="/node/4" data-drupal-link-system-path="node/4">Important dates</a>
+                    </li>
+                    <li class="menu-item">
+                        <a href="/" data-drupal-link-system-path="<front>">About the Drupal Association Romania</a>
+                    </li>
+                    <li class="menu-item">
+                        <a href="/" data-drupal-link-system-path="<front>">News</a>
+                    </li>
+                </ul>
+            </li>
+            <li class="menu-item menu-item--active-trail">
+                <a href="/user/register" data-drupal-link-system-path="user/register" class="is-active action-link">Registration</a>
+            </li>
+            <li class="menu-item menu-item--expanded">
+                <a href="#" data-drupal-link-system-path="<front>" class="action-link">Program</a>
+                <ul class="menu sublinks">
+                    <li class="menu-item">
+                        <a href="/" data-drupal-link-system-path="<front>">Schedule</a>
+                    </li>
+                    <li class="menu-item">
+                        <a href="/" data-drupal-link-system-path="<front>">Birds of a Feather</a>
+                    </li>
+                    <li class="menu-item">
+                        <a href="/node/4" data-drupal-link-system-path="node/4">Sprints</a>
+                    </li>
+                    <li class="menu-item">
+                        <a href="/" data-drupal-link-system-path="<front>">Social Events</a>
+                    </li>
+                    <li class="menu-item">
+                        <a href="/" data-drupal-link-system-path="<front>">Accepted Sessions</a>
+                    </li>
+                    <li class="menu-item">
+                        <a href="/" data-drupal-link-system-path="<front>">Session Tracks</a>
+                    </li>
+                </ul>
+            </li>
+            <li class="menu-item menu-item--expanded">
+                <a href="/" data-drupal-link-system-path="<front>" class="action-link">Travel</a>
+                <ul class="menu sublinks">
+                    <li class="menu-item">
+                        <a href="/" data-drupal-link-system-path="<front>">Venue</a>
+                    </li>
+                    <li class="menu-item">
+                        <a href="/" data-drupal-link-system-path="<front>">Visit Cluj-Napoca</a>
+                    </li>
+                </ul>
+            </li>
+            <li class="menu-item menu-item--collapsed">
+                <a href="/" data-drupal-link-system-path="<front>" class="action-link">Community</a>
+            </li>
+            <li class="menu-item menu-item--expanded">
+                <a href="/" data-drupal-link-system-path="<front>" class="action-link">Sponsors</a>
+                <ul class="menu sublinks">
+                    <li class="menu-item">
+                        <a href="/" data-drupal-link-system-path="<front>">Become a sponsor</a>
+                    </li>
+                    <li class="menu-item">
+                        <a href="/" data-drupal-link-system-path="<front>">Our sponsors</a>
+                    </li>
+                </ul>
+            </li>
+            <li class="menu-item">
+                <a href="/" data-drupal-link-system-path="<front>" class="action-link">Login</a>
+            </li>
+        </ul>
+    </nav>
     <ul>
         <?php include "modules/share-links.php";?>
     </ul>
