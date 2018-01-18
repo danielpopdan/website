317c5a8aaa45bb775b82ff5bf0d8af60c1bed88a
Andrei Bajenaru <andrei.bajenaru@pitechnologies.ro>
2018-01-18 18:01:21 +0200

    Session detail page structure created. See DC #321.

---
+++ _html/templates/modules/session-detail-content.php
@@ -0,0 +1,83 @@
+<div class="session-detail-content">
+    <div class="large-title-block title">
+        <h2>Session title</h2>
+    </div>
+    <div class="l-container">
+        <div class="session-detail--item">
+            <div class="session-detail--item-term">
+                <p>Status:</p>
+            </div>
+            <div class="session-detail--item-value session-status">
+                <p>accepted</p>
+            </div>
+        </div>
+        <div class="session-detail--item">
+            <div class="session-detail--item-term">
+                <p>Speakers:</p>
+            </div>
+            <div class="session-detail--item-value session-speakers">
+                <p>Thomas Smith Timothy Simpson</p>
+            </div>
+        </div>
+        <div class="session-detail--item">
+            <div class="session-detail--item-term">
+                <p>Description:</p>
+            </div>
+            <div class="session-detail--item-value">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exerc itation ullamco laboris nisi ut aliquip ex ea comm odo consequat. Duis aute irure dolor in reprehe nderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur aute irure do.</p>
+            </div>
+        </div>
+        <div class="session-detail--item">
+            <div class="session-detail--item-term">
+                <p>Session Type:</p>
+            </div>
+            <div class="session-detail--item-value">
+                <p>session</p>
+            </div>
+        </div>
+        <div class="session-detail--item">
+            <div class="session-detail--item-term">
+                <p>Duration:</p>
+            </div>
+            <div class="session-detail--item-value">
+                <p>60'</p>
+            </div>
+        </div>
+        <div class="session-detail--item">
+            <div class="session-detail--item-term">
+                <p>Session track:</p>
+            </div>
+            <div class="session-detail--item-value">
+                <p>project management</p>
+            </div>
+        </div>
+        <div class="session-detail--item">
+            <div class="session-detail--item-term">
+                <p>Drupal version:</p>
+            </div>
+            <div class="session-detail--item-value">
+                <p>7x</p>
+            </div>
+        </div>
+        <div class="session-detail--item">
+            <div class="session-detail--item-term">
+                <p>Audience:</p>
+            </div>
+            <div class="session-detail--item-value">
+                <p>advanced</p>
+            </div>
+        </div>
+        <div class="session-detail--item">
+            <div class="session-detail--item-term">
+                <p>Slides</p>
+            </div>
+            <div class="session-detail--item-value session-presentation">
+                <a href="#" class="session-presentation--link">project name.pptx</a>
+                <span class="session-presentation--size">601.69 KB</span>
+            </div>
+        </div>
+        <div class="session-detail--presentation">
+            <img src="../images/icons/presentation-slides.png" alt="Session presentation">
+        </div>
+    </div>
+</div>
