06c796bb89d401562c0fc1642aa0054bda6c0925
vlad.gheorghita <vlad.gheorghita@pitechplus.com>
2018-01-19 16:20:03 +0200

    Meet team page. See #338.
    f

---
+++ _html/templates/modules/meet-team-content.php
@@ -0,0 +1,108 @@
+    <div class="section-intro sponsors-intro">
+        <div class="section-intro--background"></div>
+        <div class="section-intro--text">
+            <h1 class="section-intro--title">Meet the team</h1>
+            <div class="section-intro--details meet-team-text">
+                <p>It takes a teamwork to create a dreamwork. more more more more description text bla bla bla </p>
+            </div>
+        </div>
+    </div>
+    <div class="meet-main-background">
+        <div class="l-container meet-team-container">
+            <div class="homepage-speakers">
+                <div class="l-container">
+                    <div class="l-row">
+                        <div class="homepage-speakers--display l-col">
+                            <img src="../images/homepage/speakers-mentors.png" alt="speaker portrait">
+                            <div class="homepage-speakers--display--name">
+                                Popescu Ion
+                            </div>
+                            <div class="homepage-speakers--display--position">
+                                Developer
+                            </div>
+                            <div class="homepage-speakers--display--firm">
+                                PITECHPLUS
+                            </div>
+                        </div>
+                        <div class="homepage-speakers--display l-col">
+                            <img src="../images/homepage/speakers-mentors.png" alt="speaker portrait">
+                            <div class="homepage-speakers--display--name">
+                                Popescu Ion
+                            </div>
+                            <div class="homepage-speakers--display--position">
+                                Developer
+                            </div>
+                            <div class="homepage-speakers--display--firm">
+                                PITECHPLUS
+                            </div>
+                        </div>
+                        <div class="homepage-speakers--display l-col">
+                            <img src="../images/homepage/speakers-mentors.png" alt="speaker portrait">
+                            <div class="homepage-speakers--display--name">
+                                Popescu Ion
+                            </div>
+                            <div class="homepage-speakers--display--position">
+                                Developer
+                            </div>
+                            <div class="homepage-speakers--display--firm">
+                                PITECHPLUS
+                            </div>
+                        </div>
+                    </div>
+                    <div class="l-row">
+                        <div class="homepage-speakers--display l-col">
+                            <img src="../images/homepage/speakers-mentors.png" alt="speaker portrait">
+                            <div class="homepage-speakers--display--name">
+                                Popescu Ion
+                            </div>
+                            <div class="homepage-speakers--display--position">
+                                Developer
+                            </div>
+                            <div class="homepage-speakers--display--firm">
+                                PITECHPLUS
+                            </div>
+                        </div>
+                        <div class="homepage-speakers--display l-col">
+                            <img src="../images/homepage/speakers-mentors.png" alt="speaker portrait">
+                            <div class="homepage-speakers--display--name">
+                                Popescu Ion
+                            </div>
+                            <div class="homepage-speakers--display--position">
+                                Developer
+                            </div>
+                            <div class="homepage-speakers--display--firm">
+                                PITECHPLUS
+                            </div>
+                        </div>
+                        <div class="homepage-speakers--display l-col">
+                            <img src="../images/homepage/speakers-mentors.png" alt="speaker portrait">
+                            <div class="homepage-speakers--display--name">
+                                Popescu Ion
+                            </div>
+                            <div class="homepage-speakers--display--position">
+                                Developer
+                            </div>
+                            <div class="homepage-speakers--display--firm">
+                                PITECHPLUS
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</main>
+<footer class="footer meet-team-footer">
+    <div class="footer--background"></div>
+    <div class="footer--sponsor">
+        <p class="footer--sponsor--intro">Drupal community hosting powered by</p>
+        <a href="javascript:void(0)" title="Acquia">
+            <img src="../images/logos/acquia-logo.png" alt="Acquia" class="footer--sponsor--image">
+        </a>
+        <p class="diamond-sponsor-text">Sponsored by</p>
+        <div class="sponsors-wrapper">
+            <a href="#"><img src="../images/logos/civicaction.png" alt="sponsor brand" class="sponsor-logo"></a>
+            <a href="#"><img src="../images/logos/suddenlink.png" alt="sponsor brand" class="sponsor-logo"></a>
+        </div>
+    </div>
+</footer>
