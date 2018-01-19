06d461abc2121145b815c2d200febc0e64875322
Laurentiu Dancu <laurentiu.dancu@pitechnologies.ro>
2018-01-19 14:59:34 +0200

    Add products email storage and form. See DCT-341.

---
+++ docroot/modules/custom/dct_commerce/src/Plugin/Commerce/CheckoutFlow/DctMultistepFlow.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Drupal\dct_commerce\Plugin\Commerce\CheckoutFlow;
+
+use Drupal\commerce_checkout\Plugin\Commerce\CheckoutFlow\CheckoutFlowWithPanesBase;
+
+/**
+ * Provides the DCT multistep checkout flow.
+ *
+ * @CommerceCheckoutFlow(
+ *   id = "dct_multistep",
+ *   label = "DCT Multistep Flow",
+ * )
+ */
+class DctMultistepFlow extends CheckoutFlowWithPanesBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getSteps() {
+    // Note that previous_label and next_label are not the labels
+    // shown on the step itself. Instead, they are the labels shown
+    // when going back to the step, or proceeding to the step.
+    return [
+        'login' => [
+          'label' => $this->t('Login'),
+          'previous_label' => $this->t('Go back'),
+          'has_sidebar' => FALSE,
+        ],
+        'ticket_information' => [
+          'label' => $this->t('Ticket information'),
+          'has_sidebar' => TRUE,
+          'previous_label' => $this->t('Go back'),
+        ],
+        'order_information' => [
+          'label' => $this->t('Order information'),
+          'has_sidebar' => TRUE,
+          'previous_label' => $this->t('Go back'),
+          'next_label' => $this->t('Continue to order information'),
+        ],
+        'review' => [
+          'label' => $this->t('Review'),
+          'next_label' => $this->t('Continue to review'),
+          'previous_label' => $this->t('Go back'),
+          'has_sidebar' => TRUE,
+        ],
+      ] + parent::getSteps();
+  }
+
+}
---
+++ docroot/modules/custom/dct_commerce/src/Plugin/Commerce/CheckoutPane/TicketInformation.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Drupal\dct_commerce\Plugin\Commerce\CheckoutPane;
+
+use Drupal\commerce_checkout\Plugin\Commerce\CheckoutPane\CheckoutPaneBase;
+use Drupal\commerce_checkout\Plugin\Commerce\CheckoutPane\CheckoutPaneInterface;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Link;
+
+/**
+ * Provides the review pane.
+ *
+ * @CommerceCheckoutPane(
+ *   id = "ticket_information",
+ *   label = @Translation("Ticket information"),
+ *   default_step = "ticket_information",
+ * )
+ */
+class TicketInformation extends CheckoutPaneBase implements CheckoutPaneInterface {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildPaneForm(array $pane_form, FormStateInterface $form_state, array &$complete_form) {
+    $order_items = $this->order->order_items;
+    foreach ($order_items as $delta => $item) {
+      $order_item = $item->entity;
+      /* @var $order_item \Drupal\commerce_order\Entity\OrderItemInterface */
+      $pane_form['order_item'][$delta] = [
+        '#type' => 'fieldset',
+        '#title' => $order_item->getTitle(),
+        '#tree' => TRUE,
+      ];
+      $quantity = (int) $order_item->getQuantity();
+      for ($number = 0; $number < $quantity; $number++) {
+        $recipient_item_field = $order_item->field_recipients->get($number);
+        $recipient = isset($recipient_item_field) ? $recipient_item_field->value : '';
+        $pane_form['order_item'][$delta][$number] = [
+          '#type' => 'email',
+          '#title' => t('Ticket recipient email #%number', ['%number' => $number + 1]),
+          '#default_value' => $recipient,
+        ];
+      }
+    }
+
+    return $pane_form;
+  }
+
+  public function submitPaneForm(array &$pane_form, FormStateInterface $form_state, array &$complete_form) {
+    $values = $form_state->getValue('ticket_information');
+    foreach ($values['order_item'] as $delta => $item_values) {
+      /* @var $order_item_entity \Drupal\commerce_order\Entity\OrderItemInterface */
+      $order_item_entity = $this->order->order_items[$delta]->entity;
+      $order_item_entity->set('field_recipients', $item_values);
+      $order_item_entity->save();
+    }
+  }
+
+}
