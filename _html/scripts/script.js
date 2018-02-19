32e410ccf5e421589783233b9fd7e6c4bf838cd5
Andrei Bajenaru <andrei.bajenaru@pitechnologies.ro>
2018-02-19 11:38:43 +0200

    Schedule page structure with slider on mobile. See DC-491.

--- _html/scripts/script.js
+++ _html/scripts/script.js
@@ -288,6 +288,55 @@
                 return false;
             });
         },
+
+        scheduleTabs: function() {
+            $('.day-tabs-container .day-tab').on('click', function() {
+                var tabId = $(this).attr('data-tab');
+
+                $('.day-tabs-container .day-tab').removeClass('active-tab');
+                $('.day-tab-content').removeClass('active-tab-content');
+
+                $(this).addClass('active-tab');
+                $('#' + tabId).addClass('active-tab-content');
+            });
+        },
+
+        mobileScheduleSlider: function() {
+            var roomSlider = $('.mobile-room-slider');
+            var daySlider = $('.day-schedule');
+
+            var roomSliderOptions = {
+                infinite: false,
+                arrows: true,
+                autoplay: false,
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                asNavFor: '.day-schedule'
+            };
+
+            var dayScheduleOptions = {
+                infinite: false,
+                arrows: false,
+                autoplay: false,
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                asNavFor: '.mobile-room-slider'
+            }
+
+            if($(window).width() < 768) {
+                roomSlider.slick(roomSliderOptions);
+                daySlider.slick(dayScheduleOptions);
+
+                $('.day-tab').on('click', function() {
+                    if(roomSlider.hasClass('slick-initialized') || daySlider.hasClass('slick-initialized')) {
+                        roomSlider.slick('unslick');
+                        daySlider.slick('unslick');
+                        daySlider.slick(dayScheduleOptions);
+                        roomSlider.slick(roomSliderOptions);
+                    }
+                });
+            }
+        }
     };
 
 
@@ -301,5 +350,11 @@
         dCampTrans.locationMap();
         dCampTrans.sponsorsMatchHeight();
         dCampTrans.burgerMenu();
+        dCampTrans.scheduleTabs();
+        dCampTrans.mobileScheduleSlider();
+    });
+
+    $(window).resize(function() {
+        dCampTrans.mobileScheduleSlider();
     });
 })(jQuery);
