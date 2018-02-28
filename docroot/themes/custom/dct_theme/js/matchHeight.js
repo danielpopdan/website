d1fa9bfa7f65d364089117aad9a19aa11ab19cb8
Daniel Popdan <dpopdan@pitechnologies.ro>
2018-02-28 13:27:32 +0200

    Add match height on sponsors listing. See #505.

--- docroot/themes/custom/dct_theme/js/matchHeight.js
+++ docroot/themes/custom/dct_theme/js/matchHeight.js
@@ -18,6 +18,9 @@
       var matchHeight = function() {
         var sponsorCategory = $('.sponsors-categories--category');
         sponsorCategory.matchHeight();
+
+        var sponsorImage = $('.l-col .field--name-field-short-description');
+        sponsorImage.matchHeight();
       };
 
       matchHeight();
