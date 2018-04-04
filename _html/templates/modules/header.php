03c4dfbad40dabf6f0d450bb0038a4129b3fbe0c
Andrei Bajenaru <andrei.bajenaru@pitechnologies.ro>
2018-04-04 14:59:12 +0300

    Structural changes for menu. See DC-540.

--- _html/templates/modules/header.php
+++ _html/templates/modules/header.php
@@ -3,20 +3,14 @@
         <a href="javascript:void(0)" title="Homepage"></a>
     </div>
     <div class="header--menu link-menu">
+        <?php include "modules/burger-menu.php";?>
         <nav>
             <ul class="menu">
                 <li class="menu-item">
-                    <a href="javascript:void(0)" title="Menu 1" class="header--menu--link">Menu 1</a>
-                </li>
-                <li class="menu-item">
-                    <a href="javascript:void(0)" title="Menu 2" class="header--menu--link">Menu 2</a>
-                </li>
-                <li class="menu-item">
-                    <a href="javascript:void(0)" title="Log in" class="header--menu--link">Log in</a>
+                    <a href="javascript:void(0)" title="Log in" class="header--menu--link">Buy a ticket</a>
                 </li>
             </ul>
         </nav>
     </div>
-    <?php include "modules/burger-menu.php";?>
 </header>
 <main>
