4f8a0570bf3bce78da970696c69b04e4c9eebf49
Daniel Popdan <dpopdan@pitechplus.com>
2019-11-18 15:51:27 +0200

    Update local docker settings.

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -40,4 +40,5 @@ if (file_exists($app_root . '/' . $site_path . '/settings.platformsh.php')) {
 // Local settings. These come last so that they can override anything.
 if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
     include $app_root . '/' . $site_path . '/settings.local.php';
-}$settings['install_profile'] = 'config_installer';
+}
+$settings['install_profile'] = 'standard';
