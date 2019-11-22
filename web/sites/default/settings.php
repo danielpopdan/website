b9e67571803db18514fef6d9e0da8dbbe92f10c7
Daniel Popdan <dpopdan@pitechplus.com>
2019-11-22 15:00:18 +0200

    Update multisites.

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -35,7 +35,7 @@ $platformsh_enable_redis = false;
 // Set up a config sync directory.
 //
 // This is defined inside the read-only "config" directory, deployed via Git.
-$config_directories[CONFIG_SYNC_DIRECTORY] = '../config/sync/' . $platformsh_subsite_id;
+$config_directories[CONFIG_SYNC_DIRECTORY] = '../config/sync/';
 
 // Automatic Platform.sh settings.
 if (file_exists($app_root . '/' . $site_path . '/../settings.platformsh.php')) {
